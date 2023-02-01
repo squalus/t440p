fb4e2cf373f91849ce3be7bb7a8644ee24ba056f
squalus <squalus@squalus.net>
2023-01-31 22:14:35 -0800

    toolchain: remove unnecesary command

--- toolchain/default.nix
+++ toolchain/default.nix
@@ -1,7 +1,4 @@
 { callPackage, coreboot-toolchain }:
 coreboot-toolchain.i386.overrideAttrs (old: {
   src = (callPackage (import ../coreboot-src.nix) {});
-  buildPhase = old.buildPhase + ''
-    make iasl CPUS=$NIX_BUILD_CORES DEST=$out
-  '';
 })
