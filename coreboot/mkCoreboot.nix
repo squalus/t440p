add00c88261c4a9df72c913fe1d9069412d56146
squalus <squalus@squalus.net>
2023-03-17 07:51:34 -0700

    fix suspend problems
    - commit coreboot config instead of using the one from libreboot
    - commit ifd.bin from a donor laptop, since libreboot's seems to break
      the suspend feature

--- coreboot/mkCoreboot.nix
+++ coreboot/mkCoreboot.nix
@@ -26,9 +26,14 @@ stdenv.mkDerivation {
     patchShebangs util/xcompile/xcompile
     patchShebangs util/genbuild_h/genbuild_h.sh
     patchShebangs util/scripts/ucode_h_to_bin.sh
+    patchShebangs util/me_cleaner/me_cleaner.py
 
     cp ${config} .config
     chmod -R u+w .
+  '';
+
+  preConfigure = ''
+    make oldconfig
   '';
 
   buildPhase = ''
@@ -38,6 +43,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mv build/coreboot.rom $out
   '';
+
   passthru = {
     inherit config;
   };
--- coreboot/t440p.nix
+++ coreboot/t440p.nix
@@ -4,18 +4,13 @@ let config = stdenv.mkDerivation {
   src = libreboot;
   dontBuild = true;
   installPhase = ''
-    cp resources/coreboot/t440p_12mb/config/libgfxinit_corebootfb $out
-    sed -i '/CONFIG_GBE_BIN_PATH=/d' $out
-    sed -i '/CONFIG_IFD_BIN_PATH=/d' $out
-    sed -i '/CONFIG_ME_BIN_PATH=/d' $out
-    sed -i '/CONFIG_MRC_FILE=/d' $out
+    cp ${./config} $out
+    chmod u+w $out
 
     echo 'CONFIG_GBE_BIN_PATH="${libreboot}/blobs/t440p/gbe.bin"' >> $out
-    echo 'CONFIG_IFD_BIN_PATH="${libreboot}/blobs/t440p/ifd.bin"' >> $out
+    echo 'CONFIG_IFD_BIN_PATH="${./ifd.bin}"' >> $out
     echo 'CONFIG_ME_BIN_PATH="${me}"' >> $out
     echo 'CONFIG_MRC_FILE="${mrc}"' >> $out
-
-    sed -i 's,src/mainboard/$(MAINBOARDDIR)/data.vbt,src/mainboard/$(MAINBOARDDIR)/variants/$(CONFIG_VARIANT_DIR)/data.vbt,g' $out
   '';
   };
 in
