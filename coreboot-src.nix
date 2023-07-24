eefa05c7fbb5e9bb29bfe4db0f3e07e6f8c9f1fb
squalus <squalus@squalus.net>
2023-07-24 16:22:48 -0700

    fix coreboot hash

--- coreboot-src.nix
+++ coreboot-src.nix
@@ -3,7 +3,7 @@ fetchgit {
   url = "https://review.coreboot.org/coreboot";
   # todo - extract from libreboot resources/coreboot/default/board.cfg
   rev = "e70bc423f9a2e1d13827f2703efe1f9c72549f20"; # libreboot's rev
-  hash = "sha256-5F2XNqAec1+4Q8FiZy4rQwszvZwnn6k7+J+eUVdpR4Q=";
+  hash = "sha256-lisBjoBKsgQOtjINZKFUmn9GdTAyrgvUBIXKu73mGiE=";
   fetchSubmodules = true;
   leaveDotGit = true;
   postFetch = ''
